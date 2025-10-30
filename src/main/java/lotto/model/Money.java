7a15d093b52208a20c28f35e3d8d9233aacaad7f
soyul <soyul9280@gmail.com>
2025-10-30 19:58:42 +0900

    feat: 구매금액이 0인 경우 예외 처리

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -14,5 +14,8 @@ public class Money {
         if(amount < 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_MINUS.getMessage());
         }
+        if (amount == 0) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MONEY_ZERO.getMessage());
+        }
     }
 }
