a9c7704df1499988f6d2d3ae4418ac3c218a86c5
soyul <soyul9280@gmail.com>
2025-10-30 19:56:16 +0900

    refactor: MoneyTest 수정
    - 검증함수에서 생성자 방식으로 변경

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -3,6 +3,8 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import lotto.message.ErrorMessage;
+import lotto.model.Money;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,14 +16,14 @@ public class MoneyTest {
     @DisplayName("정상: 구입 금액이 정상적인 경우")
     public void Valid_Money_Success(int price) {
         assertThat(new Money(price))
-                .isInstanceOf(Moeny.class);
+                .isInstanceOf(Money.class);
     }
 
     @ParameterizedTest(name = "[case] {0}")
     @ValueSource(ints = {-1000, -9999})
     @DisplayName("예외: 구입 금액이 음수인 경우")
     public void InvalidMoney_Minus_Fail(int price) {
-        assertThatThrownBy(() -> money.validateMoney(price))
+        assertThatThrownBy(() ->new Money(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY_MINUS.getMessage());
     }
@@ -29,7 +31,7 @@ public class MoneyTest {
     @Test
     @DisplayName("예외: 구입 금액이 0인 경우")
     public void InvalidMoney_Zero_Fail() {
-        assertThatThrownBy(()->money.validateMoney(0))
+        assertThatThrownBy(()->new Money(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MOENY_ZERO.getMessage());
     }
@@ -37,7 +39,7 @@ public class MoneyTest {
     @Test
     @DisplayName("예외: 구입 금액이 1000단위가 아닌 경우")
     public void InvalidMoney_Not_Thousand_Fail() {
-        assertThatThrownBy(() -> money.validateMoney(29500))
+        assertThatThrownBy(() ->new Money(29500))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.INVALID_MONEY_NOT_THOUSAND_UNIT.getMessage());
     }
