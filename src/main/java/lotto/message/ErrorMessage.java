3b99a8b84770019740ec74e74dd96acbe13b3c72
soyul <soyul9280@gmail.com>
2025-10-30 22:46:21 +0900

    feat: 당첨번호가 ,로 시작하거나 끝나는 경우 예외 처리

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
     INVALID_MONEY_ZERO("구매 금액에 0이 올 수 없습니다."),
     INVALID_MONEY_NOT_THOUSAND_UNIT("구매 금액은 1,000단위로 입력해주세요."),
 
-    INVALID_WINNING_NUMBER_NULL("당첨 번호에 null이 올 수 없습니다.");
+    INVALID_WINNING_NUMBER_NULL("당첨 번호에 null이 올 수 없습니다."),
+    INVALID_COMMA_POSITION(", 는 시작과 끝에 올 수 없습니다.");
 
 
     private final String message;
--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -5,6 +5,7 @@ import lotto.message.ErrorMessage;
 
 public final class InputValidator {
     private static final Pattern NOT_NUMBER_PATTERN = Pattern.compile("[^0-9]");
+    private static final Pattern ONLY_COMMA_PATTERN = Pattern.compile(",");
 
     private InputValidator() {
     }
@@ -17,6 +18,7 @@ public final class InputValidator {
 
     public static void validateWinningNumber(String winningNumber) {
         validateNull(winningNumber);
+        validateCommaPosition(winningNumber);
     }
 
     private static void validateNull(String input) {
@@ -34,6 +36,13 @@ public final class InputValidator {
     private static void validateMoneyNumberFormat(String input) {
         if(NOT_NUMBER_PATTERN.matcher(input).find()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_NOT_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateCommaPosition(String winningNumber) {
+        String commaPattern = ONLY_COMMA_PATTERN.pattern();
+        if(winningNumber.startsWith(commaPattern)|| winningNumber.endsWith(commaPattern)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_COMMA_POSITION.getMessage());
         }
     }
 }
