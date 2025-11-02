3d24a1481ac7bc442b5f2ad0f5752636f06a9da9
soyul <soyul9280@gmail.com>
2025-11-02 15:26:48 +0900

    feat: 보너스 번호 input 초기화 제작

--- src/main/java/lotto/LottoController.java
+++ src/main/java/lotto/LottoController.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.model.BonusNumber;
 import lotto.model.Lottos;
 import lotto.model.Money;
+import lotto.model.WinningNumber;
 import lotto.utils.TypeConverter;
 import lotto.view.InputView;
 
@@ -12,13 +14,25 @@ public class LottoController {
 
     public void startLotto() {
         Money money = initMoney();
-        Lottos.create(money);
-
+        Lottos lottos = Lottos.create(money);
+        initWinningNumber();
+        initBonusNumber();
     }
 
     private static Money initMoney() {
         String amount = InputView.inputMoney();
         int amountNumber = TypeConverter.changeToNumber(amount);
         return Money.create(amountNumber);
+    }
+
+    private static WinningNumber initWinningNumber() {
+        String winningNumberInput = InputView.inputWinningNumber();
+        return WinningNumber.create(winningNumberInput);
+    }
+
+    private static BonusNumber initBonusNumber() {
+        String bonusNumberInput = InputView.inputBonusNumber();
+        int bonusNumber = TypeConverter.changeToNumber(bonusNumberInput);
+        return BonusNumber.create(bonusNumber);
     }
 }
