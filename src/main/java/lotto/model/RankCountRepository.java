c56bd38fca9383ca5dfd0a7cc1ed706245d92ac7
soyul <soyul9280@gmail.com>
2025-11-03 02:08:16 +0900

    feat: 랭크 횟수 repository 기능 제작

---
+++ src/main/java/lotto/model/RankCountRepository.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RankCountRepository {
+    private final Map<Rank, Integer> rankCounts;
+    private RankCountRepository() {
+        this.rankCounts = new HashMap<>();
+    }
+
+    public static RankCountRepository create() {
+        return new RankCountRepository();
+    }
+
+    public void addCountByRank(Rank rank) {
+        rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+    }
+}
